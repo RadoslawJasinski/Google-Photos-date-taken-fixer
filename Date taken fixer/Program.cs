0389191fe540d270fb8582e4cc8b1a00f6ff8f08
Radoslaw <Radoslaw@DESKTOP-M1H2D2A>
2024-02-06 17:59:33 +0100

    Add project files.

---
+++ Date taken fixer/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization;
+using System;
+using ExifLibrary;
+using System.IO;
+
+namespace Date_taken_fixer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj sciezke zdjec:");
+            string? folderPath = Console.ReadLine();
+
+            try
+            {
+                string[] files = Directory.GetFiles(folderPath);
+
+                foreach (string filePath in files)
+                {
+                    if (File.Exists(filePath))
+                    {
+                        DateTime modifiedDate = File.GetLastWriteTime(filePath);
+
+                        File.SetCreationTime(filePath, modifiedDate);
+                        File.SetLastWriteTime(filePath, modifiedDate);
+                        File.SetLastAccessTime(filePath, modifiedDate);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("File not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+        }
+
+    }
+}
